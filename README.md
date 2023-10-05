eb7644b04e56be2cf088a31ea28bd5114ee98ca4
Hieudinhpro <hieudinh13082002@gmail.com>
2023-10-05 09:14:43 +0700

    dev app

---
+++ README.md
@@ -0,0 +1,3 @@
+docker pull hieudinhpro/school_sentiment_analysts:v1
+
+docker run -p 8501:8501 hieudinhpro/school_sentiment_analysts:v1
---
+++ app/app.py
@@ -0,0 +1,28 @@
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+import streamlit as st
+from PIL import Image
+
+
+
+st.title("School Sentiment Analysis")
+
+st.markdown('''
+    :red[Type] :orange[something] :green[here]  .''')
+
+model = pipeline("text-classification", model="hieudinhpro/BERT_Sentiment_Vietnamese")
+
+text = st.text_area("")
+
+predi_Bt = st.button("Predict")
+if predi_Bt:
+    predict = model (text)
+    st.warning(predict)
+    
+
+    
+image = Image.open('image/hg.png')
+
+st.image(image, caption='you can try on huggingface UI')
+
+
